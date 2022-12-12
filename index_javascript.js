97ceff508face088251049217740b1302f3eb8a4
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2022-12-12 12:27:05 +0900

    Add files via upload

---
+++ index_javascript.js
@@ -0,0 +1,9 @@
+function over_change(obj) {
+  obj.style.backgroundColor = "orchid";
+  obj.style.transform = "translateY(-25px)";
+}
+
+function out_change(obj) {
+  obj.style.backgroundColor = "white";
+  obj.style.transform = "translateY(0px)";
+}
